fe09bb72865a38d0671c391633a31f73f5319a60
Niteeshkumar07 <niteeshkumar4020@gmail.com>
2025-12-20 15:21:42 +0530

    Mockito

---
+++ src/test/java/com/example/alpha/service/UserDetailsServiceImplTests.java
@@ -0,0 +1,47 @@
+package com.example.alpha.service;
+
+import com.example.alpha.Entity.User;
+import com.example.alpha.Repository.UserRepository;
+import com.example.alpha.Service.UserDetailsServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+
+public class UserDetailsServiceImplTests {
+    // In this we use Mockito as we use Testing we application start
+    // componenets loads but we mock create a dummy like as we in this real class have
+    // loadbyusername func in which i autowired repo but i dont want as if not check user present or not but below functionality
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    private UserRepository userRepository;
+
+
+    // We do beacuse to test we need repo instance without running our application
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void loadUserByUsernameTests(){
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("innntn").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailsService.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+    }
+
+}
